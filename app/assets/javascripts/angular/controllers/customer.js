eb631f8a587ce4fe1b17fb55981e7136c8a424e1
Zachery Hostens <zacheryph@gmail.com>
2013-04-26 13:32:53 -0500

    "working" s2. only works on first select currently
    also is using static data for now

--- app/assets/javascripts/angular/controllers/customer.js
+++ app/assets/javascripts/angular/controllers/customer.js
@@ -1,81 +1,10 @@
 function CustomerCtrl($scope, $location, Customers, Customer) {
-  console.log('CustomerCtrl');
-  $scope.counter = 0;
-  console.log($scope.counter);
-  $scope.customer = {name: 'guest', last: 'visitor'};
-
-  $scope.group = 'Dummy Content';
-
-  // $scope.$watch('version4', function(newVal,oldVal){
-  //   console.log("--- suck it ----");
-  //   console.log(newVal,oldVal);
-  // });
-
-  $scope.select2GroupConfig = {
-    formatResult: function(item) {
-      console.log("format result");
-      console.log(item);
-      console.log(item.customer.first_name);
-      // return item.customer;
-      return "Fakeed";
-    },
-    formatSelection: function(item) {
-      console.log("format selection");
-      console.log(item);
-      // console.log(item.customer.first_name);
-      // return item.customer.first_name;
-      return "ballz";
-    },
-    ajax: {
-      dataType: 'json',
-      url: '/customers/lookup_customer',
-      data: function (term, page) {
-        return { q: term };
-      },
-      results: function (data, page) {
-        return { results: data };
-      }
-    }
+  $scope.customers = {
+    data: [
+      {id: 1, username: "jacklin10",
+        name: "Joe Acklin", email: "jacklin10@gmail.com", text: "Joe Acklin"},
+      {id: 1, username: "zacheryph",
+        name: "Zachery Hostens", email: "zacheryph@gmail.com", text: "Zachery Hostens"}
+    ]
   };
-
-  $scope.select2GroupConfig.initSelection = function ( el, fn ) {
-    fn({ id: 2, text: 'Some group' });
-  };
-
-
-  // $scope.version4 = {
-  //   data: function() {
-  //      return {'results': $scope.data};
-  //   },
-  //   id: function (e) { return '1' },
-  //   placeholder: 'Find Customer',
-  //   formatResult: function(item) {
-  //     console.log("format result");
-  //     console.log(item);
-  //     // return item.customer;
-  //     return item.customer.first_name;
-  //   },
-  //   formatSelection: function(item) {
-  //     console.log("format selection");
-  //     $scope.ballz = item.customer.first_name;
-  //     $scope.version4model = ["idk"];
-  //     return item.customer.first_name;
-  //   },
-  //   ajax: {
-  //     dataType: 'json',
-  //     url: '/customers/lookup_customer',
-  //     data: function (term, page) {
-  //       return { q: term, page_limit: 10 };
-  //     },
-  //     results: function (data, page) {
-  //       console.log("results");
-  //       console.log(data);
-  //       // console.log(data[0].customer.first_name);
-  //       // $scope.customer.first_name= "donkeydick";
-  //       return { results: data };
-  //     }
-  //   }
-  // }
-
-  };
-
+};
